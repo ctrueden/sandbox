f0527db30253949139ca026120633568d0d0066c
Curtis Rueden <ctrueden@wisc.edu>
2016-04-25 15:27:49 -0500

    LotsOfMenuCommands: also make a JavaFX stage
    Looks like JavaFX scrolls really tall menus in an OK manner.

--- src/main/java/net/restlesscoder/sandbox/LotsOfMenuCommands.java
+++ src/main/java/net/restlesscoder/sandbox/LotsOfMenuCommands.java
@@ -3,9 +3,6 @@ package net.restlesscoder.sandbox;
 
 import java.awt.Button;
 import java.awt.Frame;
-import java.awt.Menu;
-import java.awt.MenuBar;
-import java.awt.MenuItem;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -16,15 +13,50 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.WindowConstants;
 
-public class LotsOfMenuCommands {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+@SuppressWarnings("restriction")
+public class LotsOfMenuCommands extends Application {
+
+	private static final int ITEM_COUNT = 200;
 
 	public static void main(final String... args) {
-		makeFrame();
+		launch(args);
+	}
+
+	@Override
+	public void start(final Stage stage) {
+		stage.setTitle("Stage");
+		final Scene scene = new Scene(new VBox(), 400, 350);
+		stage.setScene(scene);
+
+		final MenuBar menubar = new MenuBar();
+		((VBox) scene.getRoot()).getChildren().addAll(menubar);
+		final Menu file = new Menu("File");
+		menubar.getMenus().addAll(file);
+		for (int i = 0; i < ITEM_COUNT; i++) {
+			file.getItems().add(new MenuItem("Item #" + i));
+		}
+
+		stage.setX(100);
+		stage.setY(450);
+		stage.setWidth(300);
+		stage.setHeight(300);
+		stage.show();
+
+		// also make other kinds of frames
 		makeJFrame();
+		makeFrame();
 	}
 
 	private static void makeJFrame() {
-		final JFrame f = new JFrame("Hello");
+		final JFrame f = new JFrame("JFrame");
 		f.getContentPane().add(new JButton("Go"));
 
 		f.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -33,11 +65,11 @@ public class LotsOfMenuCommands {
 		f.setJMenuBar(menubar);
 		final JMenu file = new JMenu("File");
 		menubar.add(file);
-		for (int i = 0; i < 40; i++) {
+		for (int i = 0; i < ITEM_COUNT; i++) {
 			file.add(new JMenuItem("Item #" + i));
 		}
 
-		f.pack();
+		f.setBounds(450, 100, 300, 300);
 		f.setVisible(true);
 	}
 
@@ -53,16 +85,15 @@ public class LotsOfMenuCommands {
 			}
 		});
 
-		final MenuBar menubar = new MenuBar();
+		final java.awt.MenuBar menubar = new java.awt.MenuBar();
 		f.setMenuBar(menubar);
-		final Menu file = new Menu("File");
+		final java.awt.Menu file = new java.awt.Menu("File");
 		menubar.add(file);
-		for (int i = 0; i < 40; i++) {
-			file.add(new MenuItem("Item #" + i));
+		for (int i = 0; i < ITEM_COUNT; i++) {
+			file.add(new java.awt.MenuItem("Item #" + i));
 		}
 
-		f.pack();
+		f.setBounds(100, 100, 300, 300);
 		f.setVisible(true);
 	}
-
 }
