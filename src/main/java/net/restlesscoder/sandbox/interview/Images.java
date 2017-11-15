7c3aadd32eaf9dccd6ca75dabd64f8416162b9c2
Curtis Rueden <ctrueden@wisc.edu>
2017-11-15 13:06:03 -0600

    Rename image utility class; add more functions

--- src/main/java/net/restlesscoder/sandbox/interview/Images.java
+++ src/main/java/net/restlesscoder/sandbox/interview/Images.java
@@ -1,5 +1,5 @@
 
-package net.restlesscoder.sandbox.ui;
+package net.restlesscoder.sandbox.interview;
 
 import java.awt.image.BufferedImage;
 
@@ -8,10 +8,21 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.WindowConstants;
 
-public class RenderDoubleArrayAsImage {
+public class Images {
+
+	/** Generates a random noise image, normalized to [0.0, 1.0]. */
+	public static double[][] noise(int w, int h) {
+		final double[][] matrix = new double[h][w];
+		for (int y=0; y<h; y++) {
+			for (int x=0; x<w; x++) {
+				matrix[y][x] = Math.random();
+			}
+		}
+		return matrix;
+	}
 
 	/** Generates a gradient image matrix, normalized to [0.0, 1.0]. */
-	public static double[][] generate(final int w, final int h) {
+	public static double[][] gradient(final int w, final int h) {
 		final double[][] matrix = new double[h][w];
 		final double max = (w - 1) * (w - 1) + (h - 1) * (h - 1);
 		for (int y = 0; y < h; y++) {
@@ -20,6 +31,28 @@ public class RenderDoubleArrayAsImage {
 			}
 		}
 		return matrix;
+	}
+
+	/** Normalizes a matrix to [0.0, 1.0]. */
+	public static void normalize(final double[][] matrix) {
+		final int w = matrix[0].length, h = matrix.length;
+
+		// Find min and max.
+		double min = Double.POSITIVE_INFINITY;
+		double max = Double.NEGATIVE_INFINITY;
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
+				if (matrix[y][x] < min) min = matrix[y][x];
+				if (matrix[y][x] > max) max = matrix[y][x];
+			}
+		}
+
+		// Normalize to [0.0, 1.0].
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
+				matrix[y][x] = (matrix[y][x] - min) / (max - min);
+			}
+		}
 	}
 
 	/** Converts a normalized 2D double matrix to a {@link BufferedImage}. */
@@ -38,20 +71,11 @@ public class RenderDoubleArrayAsImage {
 	}
 
 	/** Displays the given {@link BufferedImage} in an image window. */
-	public static void display(final BufferedImage image) {
-		final JFrame f = new JFrame("Image");
+	public static void display(final String title, final BufferedImage image) {
+		final JFrame f = new JFrame(title);
 		f.getContentPane().add(new JLabel(new ImageIcon(image)));
 		f.pack();
 		f.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		f.setVisible(true);
-	}
-
-	public static void main(final String... args) throws Exception {
-		final int w = 300, h = 200;
-		final double[][] matrix = generate(w, h);
-
-		final BufferedImage image = makeBufferedImage(matrix);
-
-		display(image);
 	}
 }
