73e37f753020e6e4fea698794487e0509673acc5
Curtis Rueden <ctrueden@wisc.edu>
2020-06-15 17:43:38 -0500

    POM: update parent to pom-scijava 29.0.0-beta-2

--- src/main/java/net/restlesscoder/sandbox/scifio/ParseMetadata.java
+++ src/main/java/net/restlesscoder/sandbox/scifio/ParseMetadata.java
@@ -10,14 +10,16 @@ import java.io.File;
 
 import net.imagej.ImageJ;
 
+import org.scijava.io.location.FileLocation;
+
 public class ParseMetadata {
 
 	public static void main(final String... args) throws Exception {
 		ImageJ ij = new ImageJ();
 		File file = ij.ui().chooseFile(null, null);
-		String filePath = file.getAbsolutePath();
-		Format format = ij.scifio().format().getFormat(filePath);
-		Metadata metadata = format.createParser().parse(filePath);
+		FileLocation source = new FileLocation(file);
+		Format format = ij.scifio().format().getFormat(source);
+		Metadata metadata = format.createParser().parse(source);
 		System.out.println(new FieldPrinter(metadata));
 		ImageMetadata imageMeta = metadata.get(0);
 		System.out.println(imageMeta);
